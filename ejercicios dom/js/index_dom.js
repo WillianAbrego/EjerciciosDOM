495f08bd629c52d55e732881c078b9e485a5ebf4
WillianAbrego <willianantonioabrego@gmail.com>
2022-06-24 21:38:59 -0600

    tema dark/light

--- ejercicios dom/js/index_dom.js
+++ ejercicios dom/js/index_dom.js
@@ -3,6 +3,7 @@ import countdown from "./cuenta_regresiva.js";
 import hamburgerMenu from "./menu_hamburgesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall, shortcuts } from "./teclado.js";
+import darkTheme from "./tem_oscuro.js";
 
 const d = document;
 
@@ -12,6 +13,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("assets/assets_alarma.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "Jul 4, 2022 14:10:19", "Feliz Cumpleaños");
   scrollTopButton(".scroll-top-btn");
+  darkTheme(".dark-theme-btn", "dark-mode");
 });
 
 d.addEventListener("keydown", (e) => {
