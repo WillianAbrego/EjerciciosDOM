0dff8738eda300051c00fafcbb223de8b8bd9ae6
WillianAbrego <willianantonioabrego@gmail.com>
2022-08-17 15:25:45 -0600

    Ejercicio Ajax - stripe

--- ejercicios dom/ajax-ejercicios/assets/stripe-checkout.js
+++ ejercicios dom/ajax-ejercicios/assets/stripe-checkout.js
@@ -3,30 +3,71 @@ import STRIP_KEY from "./stripe-keys.js";
 const d = document,
   $tacos = d.getElementById("tacos"),
   $template = d.getElementById("taco-template").content,
-  $fragment = d.createDocumentFragment();
+  $fragment = d.createDocumentFragment(),
+  fetchOptions = {
+    headers: {
+      Authorization: `Bearer ${STRIP_KEY.secret}`,
+    },
+  };
 
-fetch("https://api.stripe.com/v1/products", {
-  headers: {
-    Authorization: `Bearer ${STRIP_KEY.secret}`,
-  },
-})
-  .then((res) => {
-    // console.log(res);
-    return res.json();
-  })
+let products, prices;
+const moneyFormat = (num) => `$${num.slice(0, -2)}.${num.slice(-2)}`;
+
+Promise.all([
+  fetch("https://api.stripe.com/v1/products", fetchOptions),
+  fetch("https://api.stripe.com/v1/prices", fetchOptions),
+])
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
   .then((json) => {
-    console.log(json);
+    // console.log(json);
+    products = json[0].data;
+    prices = json[1].data;
+    // console.log(products, prices);
+
+    prices.forEach((el) => {
+      let productData = products.filter((product) => product.id === el.product);
+      //  console.log(productData);
+
+      $template.querySelector(".taco").setAttribute("data-price", el.id);
+      $template.querySelector("img").src = productData[0].images[0];
+      $template.querySelector("img").alt = productData[0].name;
+      $template.querySelector("figcaption").innerHTML = `
+      ${productData[0].name}
+      <br>
+      ${moneyFormat(el.unit_amount_decimal)} ${el.currency}
+      `;
+
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+    $tacos.appendChild($fragment);
+  })
+  .catch((err) => {
+    console.log(err);
+    let message =
+      err.statusText || "ocurrio un error al conectarse con la api de stripe";
+    $tacos.innerHTML = `<p>Error ${err.status}:${message}</p>`;
   });
 
-fetch("https://api.stripe.com/v1/prices", {
-  headers: {
-    Authorization: `Bearer ${STRIP_KEY.secret}`,
-  },
-})
-  .then((res) => {
-    // console.log(res);
-    return res.json();
-  })
-  .then((json) => {
-    console.log(json);
-  });
+d.addEventListener("click", (e) => {
+  //console.log(e.target);
+  if (e.target.matches(".taco *")) {
+    let price = e.target.parentElement.getAttribute("data-price");
+    //console.log(priceId);
+    Stripe(STRIP_KEY.public)
+      .redirectToCheckout({
+        lineItems: [{ price, quantity: 1 }],
+        mode: "subscription",
+        successUrl:
+          "http://127.0.0.1:5500/ejercicios%20dom/ajax-ejercicios/assets/stripe-success.html",
+        cancelUrl:
+          "http://127.0.0.1:5500/ejercicios%20dom/ajax-ejercicios/assets/stripe-cancel.html",
+      })
+      .then((res) => {
+        if (res.error) {
+          console.log(res.error);
+          $tacos.insertAdjacentHTML("afterend", res.error.message);
+        }
+      });
+  }
+});
