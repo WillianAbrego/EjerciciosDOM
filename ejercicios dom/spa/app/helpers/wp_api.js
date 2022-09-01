5c75fafeb7cac8ab188685343f386522131dfac5
WillianAbrego <willianantonioabrego@gmail.com>
2022-09-01 16:01:08 -0600

    Ejercicio Ajax - SPA/helper peticion ajax

--- ejercicios dom/spa/app/helpers/wp_api.js
+++ ejercicios dom/spa/app/helpers/wp_api.js
@@ -1,9 +1,10 @@
-const NAME = "malvestida",
+const NAME = "css-tricks",
   DOMAIN = `https://${NAME}.com`,
   SITE = `${DOMAIN}/wp-json`,
   API_WP = `${SITE}/wp/v2`,
   POSTS = `${API_WP}/posts?_embed`,
   POST = `${API_WP}/posts`,
+  CATEGORIES = `${API_WP}/categories`,
   SEARCH = `${API_WP}/search?_embed&search=`;
 
 export default {
@@ -13,5 +14,6 @@ export default {
   API_WP,
   POSTS,
   POST,
+  CATEGORIES,
   SEARCH,
 };
