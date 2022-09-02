af67a53f7a2c004a30f81c9f640247ea291db74e
WillianAbrego <willianantonioabrego@gmail.com>
2022-09-01 18:05:06 -0600

    Ejercicio Ajax - SPA/componentes ui

--- ejercicios dom/spa/app/App.js
+++ ejercicios dom/spa/app/App.js
@@ -1,20 +1,25 @@
 import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
-
+import { Title } from "./components/Title.js";
+import { Loader } from "./components/Loader.js";
 export function App() {
-  document.getElementById("root").innerHTML = `<h1>bienvenidos a spa</h1>`;
+  //document.getElementById("root").innerHTML = `<h1>bienvenidos a spa</h1>`;
+  const d = document,
+    $root = d.getElementById("root");
+  $root.appendChild(Title());
+  $root.appendChild(Loader());
 }
 
-ajax({
-  url: api.POSTS,
-  cbSuccess: (posts) => {
-    console.log(posts);
-  },
-});
+// ajax({
+//   url: api.POSTS,
+//   cbSuccess: (posts) => {
+//     console.log(posts);
+//   },
+// });
 
-ajax({
-  url: api.CATEGORIES,
-  cbSuccess: (categories) => {
-    console.log(categories);
-  },
-});
+// ajax({
+//   url: api.CATEGORIES,
+//   cbSuccess: (categories) => {
+//     console.log(categories);
+//   },
+// });
