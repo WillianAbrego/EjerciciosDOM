92d944081035a6e6226ff1f391f946f4484e29cc
WillianAbrego <willianantonioabrego@gmail.com>
2022-09-02 16:41:49 -0600

    Ejercicio Ajax - SPA/renderizado dinamico

--- ejercicios dom/spa/app/App.js
+++ ejercicios dom/spa/app/App.js
@@ -2,21 +2,29 @@ import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
 import { Loader } from "./components/Loader.js";
 import { Header } from "./components/Header.js";
+import { Posts } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 export function App() {
   //document.getElementById("root").innerHTML = `<h1>bienvenidos a spa</h1>`;
   const d = document,
     $root = d.getElementById("root");
+
   $root.appendChild(Header());
+  $root.appendChild(Posts());
   $root.appendChild(Loader());
+
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+      let html = "";
+      posts.forEach((post) => (html += PostCard(post)));
+      d.querySelector(".loader").style.display = "none";
+      d.getElementById("posts").innerHTML = html;
+      console.log(html);
+    },
+  });
 }
-
-// ajax({
-//   url: api.POSTS,
-//   cbSuccess: (posts) => {
-//     console.log(posts);
-//   },
-// });
-
 // ajax({
 //   url: api.CATEGORIES,
 //   cbSuccess: (categories) => {
