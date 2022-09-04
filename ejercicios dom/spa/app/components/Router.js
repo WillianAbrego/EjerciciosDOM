41ba8e030382eeaa52170e0ce307c796771ef6d9
WillianAbrego <willianantonioabrego@gmail.com>
2022-09-03 23:02:12 -0600

    Ejercicio Ajax - SPA/refactorizando

--- ejercicios dom/spa/app/components/Router.js
+++ ejercicios dom/spa/app/components/Router.js
@@ -2,25 +2,37 @@ import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js";
 import { PostCard } from "./PostCard.js";
 
-export function Router() {
+export async function Router() {
   const d = document,
     w = window,
-    $posts = d.getElementById("posts");
+    $main = d.getElementById("main");
   let { hash } = location;
   console.log(hash);
 
-  $posts.innerHTML = null;
+  $main.innerHTML = null;
 
   if (!hash || hash === "#/") {
-    $posts.innerHTML = "<h2>Seccion del Home</h2>";
+    await ajax({
+      url: api.POSTS,
+      cbSuccess: (posts) => {
+        // console.log(posts);
+        let html = "";
+        posts.forEach((post) => (html += PostCard(post)));
+        //  d.querySelector(".loader").style.display = "none";
+        $main.innerHTML = html;
+        // console.log(html);
+      },
+    });
   } else if (hash.includes("#/search")) {
-    $posts.innerHTML = "<h2>Seccion del Buscador</h2>";
+    $main.innerHTML = "<h2>Seccion del Buscador</h2>";
   } else if (hash === "#/contacto") {
-    $posts.innerHTML = "<h2>Seccion del Contacto</h2>";
+    $main.innerHTML = "<h2>Seccion del Contacto</h2>";
   } else {
-    $posts.innerHTML =
+    $main.innerHTML =
       "<h2>aqui cargara el contenido del posr previamente seleccionado</h2>";
   }
+  d.querySelector(".loader").style.display = "none";
+
   //   ajax({
   //     url: api.POSTS,
   //     cbSuccess: (posts) => {
