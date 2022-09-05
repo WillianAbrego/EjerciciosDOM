edfbcb6273c0228c153f66ea7aaf2e04bfca3127
WillianAbrego <willianantonioabrego@gmail.com>
2022-09-04 23:05:29 -0600

    Ejercicio Ajax - SPA/buscador

--- ejercicios dom/spa/app/components/Router.js
+++ ejercicios dom/spa/app/components/Router.js
@@ -25,22 +25,21 @@ export async function Router() {
       },
     });
   } else if (hash.includes("#/search")) {
-    $main.innerHTML = "<h2>Seccion del Buscador</h2>";
+    let query = localStorage.getItem("wpSearch");
+    if (!query) return false;
+    await ajax({
+      url: `${api.SEARCH}${query}`,
+      cbSuccess: (search) => {
+        console.log(search);
+      },
+    });
   } else if (hash === "#/contacto") {
     $main.innerHTML = "<h2>Seccion del Contacto</h2>";
   } else {
-    $main.innerHTML =
-      "<h2>aqui cargara el contenido del posr previamente seleccionado</h2>";
     await ajax({
       url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
       cbSuccess: (post) => {
-        //console.log(post);
         $main.innerHTML = Post(post);
-        // let html = "";
-        // posts.forEach((post) => (html += PostCard(post)));
-        // //  d.querySelector(".loader").style.display = "none";
-        // $main.innerHTML = html;
-        // console.log(html);
       },
     });
   }
